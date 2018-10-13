94b4823b4767d150133461faaf7371891a9fe924
cravesoft <cravesoft@gmail.com>
2018-10-13 16:36:03 +0200

    fix display of multiple lines of text

--- src/grid/TextCell.js
+++ src/grid/TextCell.js
@@ -8,9 +8,12 @@ class TextCell extends Component {
 
   handleMouseOver() {
     let tooltip = document.getElementById('tooltip');
-    tooltip.style.top = `${this.props.yPos - 20}px`;
+    tooltip.style.bottom = `${window.innerHeight - this.props.yPos}px`;
     tooltip.style.left = `${this.props.xPos}px`;
-    tooltip.childNodes[0].innerHTML = this.props.text.value;
+    tooltip.childNodes[0].innerHTML = this.props.text.value.replace(
+      /(?:\r\n|\r|\n)/g,
+      '<br>'
+    );
     tooltip.style.visibility = 'visible';
   }
 
@@ -40,17 +43,32 @@ class TextCell extends Component {
             fill={`rgb(${text.color.r}, ${text.color.g}, ${text.color.b})`}
             fillOpacity={text.color.a}
           >
-            {text.value}
+            {text.value.split('\n').map((value, i) => {
+              return (
+                <tspan x={xPos} dy="1.2em">
+                  {value}
+                </tspan>
+              );
+            })}
           </text>
         ) : (
-          <polygon
-            onMouseOver={this.handleMouseOver}
-            onMouseOut={this.handleMouseOut}
-            fill="red"
-            points={`${xPos + size * coef},${yPos} ${xPos +
-              size},${yPos} ${xPos + size},${yPos + size * (1 - coef)} ${xPos +
-              size * coef},${yPos}`}
-          />
+          <g>
+            <rect
+              x={xPos}
+              y={yPos}
+              fill="transparent"
+              width={size}
+              height={size}
+              onMouseOver={this.handleMouseOver}
+              onMouseOut={this.handleMouseOut}
+            />
+            <polygon
+              fill="red"
+              points={`${xPos + size * coef},${yPos} ${xPos +
+                size},${yPos} ${xPos + size},${yPos +
+                size * (1 - coef)} ${xPos + size * coef},${yPos}`}
+            />
+          </g>
         )}
       </g>
     );
