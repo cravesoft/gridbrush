17887a690027a639b56c87e53ca65611bc504ef9
cravesoft <cravesoft@gmail.com>
2019-06-02 20:24:29 +0200

    Updates

---
+++ precache-manifest.6022d2860215369fa88b0191023fdc5d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efe8fc550270d2795e73c46301c4f42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65105e445c9302c6b977",
+    "url": "./static/css/main.6ea82238.chunk.css"
+  },
+  {
+    "revision": "366395c5c6ab294aa1b7",
+    "url": "./static/js/2.36ce6a48.chunk.js"
+  },
+  {
+    "revision": "65105e445c9302c6b977",
+    "url": "./static/js/main.9e514526.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0e7fec71824caa6a96133d2d9a6509d1",
+    "url": "./static/media/icon-bars.0e7fec71.svg"
+  },
+  {
+    "revision": "ce9fbfc5aa936e43a95c5eb4f0b9d317",
+    "url": "./static/media/icon-cross.ce9fbfc5.svg"
+  },
+  {
+    "revision": "567c33ac0f388361f18b4bf75395a306",
+    "url": "./static/media/icon-facebook.567c33ac.svg"
+  },
+  {
+    "revision": "4adfbdc881018f4502cfe5aa2b6149cb",
+    "url": "./static/media/icon-folder-open.4adfbdc8.svg"
+  },
+  {
+    "revision": "665b6afdc318af30c3a5d312ab6a3bba",
+    "url": "./static/media/icon-github.665b6afd.svg"
+  },
+  {
+    "revision": "99d7127482d298ef34418d5ab0989eb0",
+    "url": "./static/media/icon-link.99d71274.svg"
+  },
+  {
+    "revision": "bc05a4a62616ca0c61361650f1a1d1ec",
+    "url": "./static/media/icon-right-arrow.bc05a4a6.svg"
+  },
+  {
+    "revision": "c98f083442d0aba3a487f58c2cf08cb5",
+    "url": "./static/media/icon-twitter.c98f0834.svg"
+  }
+]);
